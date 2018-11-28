890ad162c8a6f49266a7e133666f058d5f9fec2f
DarcyG <DarcyG@gutaotekimbp.lan>
2018-11-28 15:42:09 +1100

    coming soon

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,9 +8,10 @@ export class Login extends React.Component{
     }
     render(){
         return (
-            <div>
-                <br /><br /><br />
-                <h1>Login</h1>
+            <div style={{textAlign:"center",alignContent:"center",height:"100%",width:"100%",display:"grid",position:"fixed"}}>
+                <div>
+                    <h1>Login Comming Soon</h1>
+                </div>
                 <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="true" data-scope="public_profile,email" data-onlogin="checkLoginState();"></div>
             </div>
         );
--- src/components/Programs.js
+++ src/components/Programs.js
@@ -3,9 +3,10 @@ import React from "react";
 export class Programs extends React.Component{
     render(){
         return (
-            <div>
-                <br /><br /><br />
-                <h1>Programs</h1>
+            <div style={{textAlign:"center",alignContent:"center",height:"100%",width:"100%",display:"grid",position:"fixed"}}>
+                <div>
+                    <h1>Programs Comming Soon</h1>
+                </div>
             </div>
         );
     }
